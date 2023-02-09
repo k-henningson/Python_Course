c71fbfd3dde89c68b3037aebf525f258c31bb2d4
Kyra Henningson <kyrahenningson@hotmail.com>
2023-02-08 16:20:19 -0800

    loops exercise

--- Ch2 - Basics/loops_start.py
+++ Ch2 - Basics/loops_start.py
@@ -8,19 +8,34 @@ def main():
     x = 0
 
     # TODO: define a while loop
+    while(x < 5):
+        print(x)
+        x = x  + 1
 
 
     # TODO: define a for loop
+    for x in range(5, 10):
+        print(x)
 
 
     # TODO: use a for loop over a collection
     days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    for day in days:
+        print(day)
 
 
     # TODO: use the break and continue statements
+    for x in range(5, 10):
+        # if x == 7:
+        #     break
+        if x % 2 == 0:
+            continue
+        print(x)
 
 
     # TODO: using the enumerate() function to get index 
+    for index, day in enumerate(days):
+        print (index, day)
     
   
 if __name__ == "__main__":
